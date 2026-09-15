dd7c94fdf4c33ab35ee2bbb419af3959714c5cdc
newrav1k <kritsky.academi@gmail.com>
2026-09-15 19:41:51 +0300

    Add simple web application on java

---
+++ web-service/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.1.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.newrav1k.devops"
+version = "0.0.1-SNAPSHOT"
+description = "web-service"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ web-service/src/test/java/io/newrav1k/devops/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.newrav1k.devops.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
